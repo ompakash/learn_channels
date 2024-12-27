b854dd26976b4840d2b2004a5a59da6a7952c141
Om Prakash <opytc2@gmail.com>
2024-12-28 00:04:58 +0530

    checking same logic for asyncronous

--- myapp/consumers.py
+++ myapp/consumers.py
@@ -40,6 +40,7 @@ class MySyncConsumer(SyncConsumer):
 class MyAsyncConsumer(AsyncConsumer):
     async def websocket_connect(self,event):
         print("Websocket connect event",event)
+        await self.channel_layer.group_add("programmers",self.channel_name)
         await self.send({
             "type":"websocket.accept"
         })
@@ -47,13 +48,22 @@ class MyAsyncConsumer(AsyncConsumer):
     async def websocket_receive(self,event):    
         print("Websocket receive event",event)
         print('Message:',event['text'])
-        for i in range(10):
-            await asyncio.sleep(1)
-            await self.send({
-                "type":"websocket.send",
-                "text":str(i)
-            })
+        await self.channel_layer.group_send("programmers",{
+            "type":"chat.message",
+            "message":event['text']
+        })
+         
+    async def chat_message(self,event):
+        print('event:',event)
+        await self.send({
+            "type":"websocket.send",
+            "text":event['message']
+        })
+        
         
     async def websocket_disconnect(self,event):
         print("Websocket disconnect event",event)
+        print("Channel layer:",self.channel_layer)
+        print("Channel name:",self.channel_name)
+        await self.channel_layer.group_discard("programmers",self.channel_name)
         raise StopConsumer()
